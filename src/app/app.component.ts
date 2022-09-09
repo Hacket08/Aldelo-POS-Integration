018d55a7bd2344e31dd8ff60ffbf55c85382f13b
Hacket08 <joeybryan.dante@gmail.com>
2022-09-09 16:24:09 +0800

    update xxxxxx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'Aldelo Integration Backend Portal';
+  title = 'xxxxxx';
 
   constructor(
     private router: Router,
